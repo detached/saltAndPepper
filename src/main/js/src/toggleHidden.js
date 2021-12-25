6d8e5b4f955cf9b335befc12f34f89c82aefae81
Simon Weis <simon@w3is.de>
2021-12-25 20:57:37 +0100

    Reduce size of hidden upload box

--- src/main/js/src/toggleHidden.js
+++ src/main/js/src/toggleHidden.js
@@ -3,7 +3,9 @@ export function toggleHidden(elementId) {
     var visibility = element.style.visibility;
     if (visibility == 'visible') {
        element.style.visibility = 'hidden';
+       element.style.height = '0px';
     } else {
        element.style.visibility = 'visible';
+       element.style.height = 'auto';
     }
 }
