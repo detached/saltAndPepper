12f40090c28090bec269016c90d13770b47929e8
Simon Weis <simon@w3is.de>
2022-12-04 17:43:30 +0100

    Fix axiosConfig

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const saltAndPepperClient = axios.create({
   withCredentials: true,
-  //baseURL: "/api/",
+  baseURL: "/api/",
   //baseURL: "http://localhost:8080/api/",
-  baseURL: "/",
 });
