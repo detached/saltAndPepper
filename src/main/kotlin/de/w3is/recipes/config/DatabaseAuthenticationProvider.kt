493eb4487ae7e796d719c6e5fd8ce5438072cc7b
Simon Weis <simon@w3is.de>
2024-06-02 14:24:50 +0200

    Replace deprecated micronaut api

--- src/main/kotlin/de/w3is/recipes/config/DatabaseAuthenticationProvider.kt
+++ src/main/kotlin/de/w3is/recipes/config/DatabaseAuthenticationProvider.kt
@@ -4,45 +4,39 @@ import de.w3is.recipes.users.UserRepository
 import de.w3is.recipes.users.model.PlainPassword
 import io.micronaut.http.HttpRequest
 import io.micronaut.security.authentication.AuthenticationFailureReason
-import io.micronaut.security.authentication.AuthenticationProvider
 import io.micronaut.security.authentication.AuthenticationRequest
 import io.micronaut.security.authentication.AuthenticationResponse
+import io.micronaut.security.authentication.provider.HttpRequestAuthenticationProvider
 import jakarta.inject.Singleton
-import org.reactivestreams.Publisher
 import org.slf4j.LoggerFactory
-import reactor.core.publisher.Mono
 
 @Singleton
-class DatabaseAuthenticationProvider(private val userRepository: UserRepository) :
-    AuthenticationProvider<HttpRequest<*>> {
-
+class DatabaseAuthenticationProvider<B>(private val userRepository: UserRepository) :
+    HttpRequestAuthenticationProvider<B> {
     private val logger = LoggerFactory.getLogger(DatabaseAuthenticationProvider::class.java)
 
     override fun authenticate(
-        httpRequest: HttpRequest<*>?,
-        authenticationRequest: AuthenticationRequest<*, *>,
-    ): Publisher<AuthenticationResponse> = Mono.create { emitter ->
-
-        val username = authenticationRequest.identity as String
-        val plainPassword = PlainPassword(authenticationRequest.secret as String)
+        requestContext: HttpRequest<B>,
+        authRequest: AuthenticationRequest<String, String>,
+    ): AuthenticationResponse {
+        val username = authRequest.identity as String
+        val plainPassword = PlainPassword(authRequest.secret as String)
 
         val user = userRepository.findUser(username)
 
         if (user != null) {
             if (user.authenticate(plainPassword)) {
-                emitter.success(
-                    AuthenticationResponse.success(
-                        user.id.value,
-                        listOf(user.role.name),
-                    ),
+                return AuthenticationResponse.success(
+                    user.id.value,
+                    listOf(user.role.name),
                 )
             } else {
                 logger.warn("Login failed: Password incorrect for $username")
-                emitter.error(AuthenticationResponse.exception(AuthenticationFailureReason.CREDENTIALS_DO_NOT_MATCH))
+                throw AuthenticationResponse.exception(AuthenticationFailureReason.CREDENTIALS_DO_NOT_MATCH)
             }
         } else {
             logger.warn("User not found: $username")
-            emitter.error(AuthenticationResponse.exception(AuthenticationFailureReason.USER_NOT_FOUND))
+            throw AuthenticationResponse.exception(AuthenticationFailureReason.USER_NOT_FOUND)
         }
     }
 }
